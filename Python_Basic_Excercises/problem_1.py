8617f4e332b7b7ccce2e990fd954d274e0eb37dc
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-12-08 23:53:15 +0600

    Create problem_1.py

---
+++ Python_Basic_Excercises/problem_1.py
@@ -0,0 +1,10 @@
+#Take two integer values as input and store them in two variables named X and Y. Multiply these two values and store the result in another variable named Z. Print the values of the variables: X, Y and Z.
+#input() – is used to take input.
+#print() – is used to print a value.
+
+X = int( input("Enter integer:") )
+Y = int( input("Enter integer:") )
+
+Z = X * Y
+print(X, " * ", Y , " = ", Z)
+     
