30482e7b678f6098d6617a3311e3c8d22a6c1b78
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-12-09 23:56:49 +0600

    Create problem_2.py

---
+++ Python_Basic_Excercises/problem_2.py
@@ -0,0 +1,4 @@
+# Find and print the python version that you are using. Use sys.version (module sys) or platform.python_version() (module platform) to find the version.
+
+import sys
+print("Using sys module: ", sys.version )
