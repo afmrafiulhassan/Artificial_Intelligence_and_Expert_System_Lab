5bf7fed729c4a5f27740d19d83e3f092967fce1a
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-12-13 23:52:27 +0600

    Create problem_6.py

---
+++ Python_Basic_Excercises/problem_6.py
@@ -0,0 +1,8 @@
+# Write a Python program to swap the values of two variables. Try this exercise with and without using a function.
+
+a = 10
+b = 20
+print("Before swap:\n a = ", a, "\n b = ", b)
+
+a, b = b, a
+print("After swap:\n a = ", a, "\n b = ", b)
