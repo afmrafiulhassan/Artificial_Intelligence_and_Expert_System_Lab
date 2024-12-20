80c5079e459e2c8d136152b5c44f554999a18df0
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-12-21 00:39:59 +0600

    Create problem_10.py

---
+++ Python_Basic_Excercises/problem_10.py
@@ -0,0 +1,6 @@
+#Write a Python program to count the number of occurrences of a specific character in a string. [try using count()]
+
+in_str = input("Enter a string:")
+in_char = input("Enter an alphabet to count frequency: ")
+
+print(in_str.count(in_char))
