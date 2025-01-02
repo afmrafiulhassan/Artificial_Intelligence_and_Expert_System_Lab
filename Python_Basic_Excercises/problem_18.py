ca6620b789f1f47227cc775182f423e461f50d2b
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2025-01-02 23:06:43 +0600

    problem_18.py

---
+++ Python_Basic_Excercises/problem_18.py
@@ -0,0 +1,10 @@
+# Define a function that receives a list of values and print all values of the odd indices.
+
+def print_odd_index_values(numbers):
+  for ind, val in enumerate(numbers):
+    if ind%2 != 0:
+      print(val, end=" ")
+
+  print()
+
+print_odd_index_values([1,2,3,4,5,6,7,8,9,10])
