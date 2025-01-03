da544920e605b6d3599248ee0c30fa2983cd80ca
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2025-01-03 21:51:13 +0600

    problem_21.py

---
+++ Python_Basic_Excercises/problem_21.py
@@ -0,0 +1,15 @@
+# Define a search function that receives a dictionary and a target value. Therefore, check if the target value exists in the dictionary values and return the corresponding key of the dictionary where the target value is found.
+
+def search_dictionary(myDict, target):
+  for key, val in myDict.items():
+    if val == target:
+      return key
+
+  return None
+
+targetIndex = search_dictionary({'a': 1, 'b': 2, 'd': 4, 'c': 3}, 4)
+
+if targetIndex != None:
+  print("Value found in index: ", targetIndex)
+else:
+  print("Target value not found.")
